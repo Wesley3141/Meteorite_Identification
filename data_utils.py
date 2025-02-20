1de55e5fb197b670d7267ec9b2b2f6f83c24b771
Wesley Deklich <contact@wesleydeklich.me>
2025-02-19 20:45:49 -0600

    Add files via upload

---
+++ data_utils.py
@@ -0,0 +1,21 @@
+import os
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+def prepare_data(data_dir):
+    """
+    Prepares the dataset for training and testing.
+    """
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+
+    train_dataset = datasets.ImageFolder(os.path.join(data_dir, 'train'), transform=transform)
+    test_dataset = datasets.ImageFolder(os.path.join(data_dir, 'test'), transform=transform)
+
+    train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+
+    return train_loader, test_loader
